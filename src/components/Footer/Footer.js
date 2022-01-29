2cb48ce526a040ba3d5c9a836ad204cc00d75ebe
thegoobs <guthrieschoolar95@gmail.com>
2022-01-28 18:12:34 -0600

    adjust layout styling

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,5 +2,11 @@ import React from 'react';
 import styles from './Footer.module.scss';
 
 export const Footer = () => (
-    <div className={styles.footer}>Placeholder for Footer</div>
+    <div className={styles.footer}>
+        <div>
+            Made with&nbsp;
+            <span className={styles.heart}>♥</span>
+            &nbsp;by Guthrie Schoolar
+        </div>
+    </div>
 );
